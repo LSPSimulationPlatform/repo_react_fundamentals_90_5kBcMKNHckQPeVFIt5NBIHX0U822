79224515e555ee5a7626530e2c7489236279f573
alishukur2 <yaalishukur@gmail.com>
2025-08-22 03:58:18 +0000

    my first project commited

---
+++ hello-world/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div style={{textAlign: "center"}}>
+   <h1>Hello World</h1>
+   <label style={{fontSize: "30px", color: "red"}}>It's my first project</label>
+    </div>
+  )
+}
+
+export default App;
